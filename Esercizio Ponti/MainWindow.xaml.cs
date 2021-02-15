6d6db29f6e24f3f3a4536f519099f09170912225
LorenzoLizzo <lorenzo.lizzo.stud@ispascalcomandini.it>
2021-02-15 22:25:05 +0100

    Spawn random di 4 macchine

--- Esercizio Ponti/MainWindow.xaml.cs
+++ Esercizio Ponti/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace Esercizio_Ponti
 {
@@ -20,9 +21,41 @@ namespace Esercizio_Ponti
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random r;
+        List<Image> lstImage;
+        List<Image> filaA;
+        List<Image> filaB;
         public MainWindow()
         {
             InitializeComponent();
+            r = new Random();
+            lstImage = new List<Image>();
+            filaA = new List<Image>();
+            filaB = new List<Image>();
+            RandomSpawnMacchine();
+        }
+
+        public void RandomSpawnMacchine()
+        {
+            lstImage.Add(img1);
+            lstImage.Add(img2);
+            lstImage.Add(img3);
+            lstImage.Add(img4);
+
+            foreach (Image img in lstImage)
+            {
+                if (r.Next(0, 2) == 0)
+                {
+                    img.Margin = new Thickness(181, (-50 - (60 * (filaA.Count))), 0, 0);
+                    filaA.Add(img);
+                }
+                else
+                {
+                    img.Margin = new Thickness(238, (590 + (60 * (filaB.Count))), 0, 0);
+                    filaB.Add(img);
+                }
+            }
+
         }
     }
 }
