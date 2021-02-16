1d143b8a0a5d6e6da49b492a00a2a798d69f68f9
LorenzoLizzo <lorenzo.lizzo.stud@ispascalcomandini.it>
2021-02-16 17:00:14 +0100

    Aggiunti metodi di movimento delle file

--- Esercizio Ponti/MainWindow.xaml.cs
+++ Esercizio Ponti/MainWindow.xaml.cs
@@ -21,10 +21,13 @@ namespace Esercizio_Ponti
     /// </summary>
     public partial class MainWindow : Window
     {
+        Thread t1;
+        Thread t2;
         Random r;
         List<Image> lstImage;
         List<Image> filaA;
         List<Image> filaB;
+        object locker = new object();
         public MainWindow()
         {
             InitializeComponent();
@@ -33,6 +36,11 @@ namespace Esercizio_Ponti
             filaA = new List<Image>();
             filaB = new List<Image>();
             RandomSpawnMacchine();
+            t1 = new Thread(new ThreadStart(MuoviFilaA));
+            t2 = new Thread(new ThreadStart(MuoviFilaB));
+
+            t1.Start();
+            t2.Start();
         }
 
         public void RandomSpawnMacchine()
@@ -56,6 +64,276 @@ namespace Esercizio_Ponti
                 }
             }
 
+        }
+
+        public void MuoviFilaA()
+        {
+            if (filaA.Count != 0)
+            {
+                while (true)
+                {
+                    bool ver = false;
+                    for (int i = 0; i < filaA.Count; i++)
+                    {
+                        int marginLeft = int.MinValue;
+                        int marginTop = int.MinValue;
+                        int marginBottom = int.MinValue;
+                        int marginRight = int.MinValue;
+                        Image img = filaA[i];
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            marginLeft = (int)img.Margin.Left;
+                            marginTop = (int)img.Margin.Top;
+                            marginBottom = (int)img.Margin.Bottom;
+                            marginRight = (int)img.Margin.Right;
+                        }));
+                        Thread.Sleep(TimeSpan.FromMilliseconds(100));
+                        marginTop += 30;
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            img.Margin = new Thickness(marginLeft, marginTop, marginRight, marginBottom);
+                        }));
+
+                        if (marginTop == 100)
+                        {
+                            ver = true;
+                            break;
+                        }
+                    }
+                    if (ver)
+                    {
+                        break;
+                    }
+                }
+
+                lock (locker)
+                {
+                    Queue<Image> queueImgEntrateNelPonte = new Queue<Image>();
+                    while (true)
+                    {
+                        bool ver = false;
+                        for (int i = 0; i < filaA.Count; i++)
+                        {
+                            int marginLeft = int.MinValue;
+                            int marginTop = int.MinValue;
+                            int marginBottom = int.MinValue;
+                            int marginRight = int.MinValue;
+                            Image img = filaA[i];
+                            this.Dispatcher.BeginInvoke(new Action(() =>
+                            {
+                                marginLeft = (int)img.Margin.Left;
+                                marginTop = (int)img.Margin.Top;
+                                marginBottom = (int)img.Margin.Bottom;
+                                marginRight = (int)img.Margin.Right;
+                            }));
+
+
+                            Thread.Sleep(TimeSpan.FromMilliseconds(100));
+
+                            if (marginTop == 100)
+                            {
+                                queueImgEntrateNelPonte.Enqueue(img);
+                            }
+                            marginTop += 30;
+
+                            this.Dispatcher.BeginInvoke(new Action(() =>
+                            {
+                                img.Margin = new Thickness(marginLeft, marginTop, marginRight, marginBottom);
+                            }));
+
+
+                            if (marginTop == 400)
+                            {
+                                queueImgEntrateNelPonte.Dequeue();
+                                if (queueImgEntrateNelPonte.Count == 0)
+                                {
+                                    ver = true;
+                                    break;
+                                }
+                            }
+
+                        }
+
+                        if (ver)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+
+                while (true)
+                {
+                    bool ver = false;
+                    for (int i = 0; i < filaA.Count; i++)
+                    {
+                        int marginLeft = int.MinValue;
+                        int marginTop = int.MinValue;
+                        int marginBottom = int.MinValue;
+                        int marginRight = int.MinValue;
+                        Image img = filaA[i];
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            marginLeft = (int)img.Margin.Left;
+                            marginTop = (int)img.Margin.Top;
+                            marginBottom = (int)img.Margin.Bottom;
+                            marginRight = (int)img.Margin.Right;
+                        }));
+                        Thread.Sleep(TimeSpan.FromMilliseconds(100));
+                        marginTop += 30;
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            img.Margin = new Thickness(marginLeft, marginTop, marginRight, marginBottom);
+                        }));
+
+                        if (marginTop == 640 && i == filaA.Count - 1)
+                        {
+                            ver = true;
+                            break;
+                        }
+                    }
+                    if (ver)
+                    {
+                        break;
+                    }
+                }
+
+
+            }
+        }
+        public void MuoviFilaB()
+        {
+
+            if (filaB.Count != 0)
+            {
+                while (true)
+                {
+                    bool ver = false;
+                    for (int i = 0; i < filaB.Count; i++)
+                    {
+                        int marginLeft = int.MaxValue;
+                        int marginTop = int.MaxValue;
+                        int marginBottom = int.MaxValue;
+                        int marginRight = int.MaxValue;
+                        Image img = filaB[i];
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            marginLeft = (int)img.Margin.Left;
+                            marginTop = (int)img.Margin.Top;
+                            marginBottom = (int)img.Margin.Bottom;
+                            marginRight = (int)img.Margin.Right;
+                        }));
+                        Thread.Sleep(TimeSpan.FromMilliseconds(100));
+                        marginTop -= 30;
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            img.Margin = new Thickness(marginLeft, marginTop, marginRight, marginBottom);
+                        }));
+
+                        if (marginTop == 410)
+                        {
+                            ver = true;
+                            break;
+                        }
+                    }
+                    if (ver)
+                    {
+                        break;
+                    }
+                }
+
+                lock (locker)
+                {
+                    Queue<Image> queueImgEntrateNelPonte = new Queue<Image>();
+                    while (true)
+                    {
+                        bool ver = false;
+                        for (int i = 0; i < filaB.Count; i++)
+                        {
+                            int marginLeft = int.MaxValue;
+                            int marginTop = int.MaxValue;
+                            int marginBottom = int.MaxValue;
+                            int marginRight = int.MaxValue;
+                            Image img = filaB[i];
+                            this.Dispatcher.BeginInvoke(new Action(() =>
+                            {
+                                marginLeft = (int)img.Margin.Left;
+                                marginTop = (int)img.Margin.Top;
+                                marginBottom = (int)img.Margin.Bottom;
+                                marginRight = (int)img.Margin.Right;
+                            }));
+
+
+                            Thread.Sleep(TimeSpan.FromMilliseconds(100));
+
+                            if (marginTop == 410)
+                            {
+                                queueImgEntrateNelPonte.Enqueue(img);
+                            }
+                            marginTop -= 30;
+
+                            this.Dispatcher.BeginInvoke(new Action(() =>
+                            {
+                                img.Margin = new Thickness(marginLeft, marginTop, marginRight, marginBottom);
+                            }));
+
+
+                            if (marginTop == 110)
+                            {
+                                queueImgEntrateNelPonte.Dequeue();
+                                if (queueImgEntrateNelPonte.Count == 0)
+                                {
+                                    ver = true;
+                                    break;
+                                }
+                            }
+
+                        }
+
+                        if (ver)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                while (true)
+                {
+                    bool ver = false;
+                    for (int i = 0; i < filaB.Count; i++)
+                    {
+                        int marginLeft = int.MaxValue;
+                        int marginTop = int.MaxValue;
+                        int marginBottom = int.MaxValue;
+                        int marginRight = int.MaxValue;
+                        Image img = filaB[i];
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            marginLeft = (int)img.Margin.Left;
+                            marginTop = (int)img.Margin.Top;
+                            marginBottom = (int)img.Margin.Bottom;
+                            marginRight = (int)img.Margin.Right;
+                        }));
+                        Thread.Sleep(TimeSpan.FromMilliseconds(100));
+                        marginTop -= 30;
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            img.Margin = new Thickness(marginLeft, marginTop, marginRight, marginBottom);
+                        }));
+
+                        if (marginTop == -70 && i == filaB.Count - 1)
+                        {
+                            ver = true;
+                            break;
+                        }
+                    }
+                    if (ver)
+                    {
+                        break;
+                    }
+                }
+
+            }
         }
     }
 }
